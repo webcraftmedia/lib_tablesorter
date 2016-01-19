f8158bfdea25f8fae9687e5f999d2258a05f1965
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:50:10 +0100

    fixed path

--- lib_tablesorter.php
+++ lib_tablesorter.php
@@ -4,9 +4,9 @@ class lib_tablesorter extends \LIB\lib_jscss{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/tablesorter/lib/dist/js/jquery.tablesorter.min.js');}
+        return new \SYSTEM\PLIB('/tablesorter/lib/dist/js/jquery.tablesorter.min.js');}
     public static function css_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/tablesorter/lib/dist/css/jquery.tablesorter.pager.min.css');}
+        return new \SYSTEM\PLIB('/tablesorter/lib/dist/css/jquery.tablesorter.pager.min.css');}
     public static function version(){
         return '<a href="https://github.com/Mottie/tablesorter" target="_blank">https://github.com/Mottie/tablesorter</a> (commit: ?)';}
 }
